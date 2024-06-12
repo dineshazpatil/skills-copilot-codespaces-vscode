fac37817e9d04f9ec1d403f73e4d034e7abd9fd6
dineshazpatil <42247517+dineshazpatil@users.noreply.github.com>
2024-06-12 13:38:39 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,44 +1,12 @@
 // create web server
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-var fs = require('fs');
-var path = require('path');
-var commentsPath = path.join(__dirname, 'comments.json');
 
-// enable CORS
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
-
-// parse application/json
-app.use(bodyParser.json());
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// get comments
+// create a route to handle incoming requests
 app.get('/comments', function(req, res) {
-    fs.readFile(commentsPath, 'utf8', function(err, data) {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        res.json(JSON.parse(data));
-    });
+  res.send('This is the comments page');
 });
 
-// add comment
-app.post('/comments', function(req, res) {
-    fs.readFile(commentsPath, 'utf8', function(err, data) {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        var comments = JSON.parse(data);
-        var newComment = {
-            id: Date.now(),
+// start the server on port 8000
+app.listen(8000);
+console.log('Server started on port 8000');
